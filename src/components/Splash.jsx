b27593b162ab5eea5e0396e30a982efeb495daf1
Eva Wiedmann <evawiedmann@gmail.com>
2020-03-24 20:55:02 -0700

    splash update

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 function Picker(){
   return (
     <div>
-      <h2>Choose One:</h2>
+      <h2>Welcome! Let's get started.</h2>
+      <h3>Choose One:</h3>
       <div><Link to="/MenteeForm">Mentee</Link> | <Link to="/MentorForm">Mentor</Link>
       </div>
     </div>
