ea23320a7321fadcc8b7f1d55ee0c8a81f2ab0c2
Eva Wiedmann <evawiedmann@gmail.com>
2020-03-27 12:10:38 -0700

    footer links complete, styling still needs some work

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom';
 function Footer(){
   return (
     <div>
-      <h1>Social Media Links</h1>
-      <div className='social'>
-      <a href="https://twitter.com/epicodus?lang=en">Twitter</a>
-  | <br/><a href= 'https://www.facebook.com/epicodus.school/'>Facebook</a> <a href='https://www.instagram.com/epicodus.school/?hl=en'>Instagram</a>
-      </div>
+    <h1>Social Media Links</h1>
+    <div className='social'>
+    <ul class="list-inline">
+    <li><a href="https://twitter.com/epicodus?lang=en">Twitter</a></li>
+    <li><a href= 'https://www.facebook.com/epicodus.school'>Facebook</a></li>
+    <li><a href='https://www.instagram.com/epicodus.school/?hl=en'>Instagram</a></li>
+    <li><a href='https://www.instagram.com/epicodus.school/?hl=en'>Instagram</a></li>
+    <li><a href='https://www.epicodus.com/'>Epicodus Site</a></li>
+    <li><a href= 'https://www.facebook.com/epicodus.school'>Facebook</a></li>
+    <li><a href="mailto:hi@epicodus.com">Email</a></li>
+    </ul>
+    </div>
     </div>
   );
 }
